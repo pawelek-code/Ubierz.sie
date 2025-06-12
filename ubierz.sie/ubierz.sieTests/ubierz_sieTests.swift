bd0736a1b1035444d42c926d504e32c1f27a0fa1
pawelek-code <pawel.wylezek@student.wsb.edu.pl>
2025-06-12 03:57:51 -0700

    Utworzenie projektu SwiftUI w Xcode

---
+++ ubierz.sie/ubierz.sieTests/ubierz_sieTests.swift
@@ -0,0 +1,17 @@
+//
+//  ubierz_sieTests.swift
+//  ubierz.sieTests
+//
+//  Created by Pablo on 12/06/2025.
+//
+
+import Testing
+@testable import ubierz_sie
+
+struct ubierz_sieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
